6f6868e5650dd53c43a330949b7855d84e3d4597
Maggie Kuo <perfumekuo@gmail.com>
2016-04-28 00:18:11 +0800

    4/28

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.tom.atm;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,6 +13,9 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String PREF_USERID = "PREF_USERID";
+    private static final String PREF_PASSWD = "PREF_PASSWD";
+
     @BindView(R.id.userid) EditText edUserid;
     @BindView(R.id.passwd) EditText edPasswd;
     @BindView(R.id.btnLogin) Button btnLogin;
@@ -21,6 +25,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        SharedPreferences pref = getSharedPreferences("atm",MODE_PRIVATE);
+        edUserid.setText(pref.getString(PREF_USERID, ""));
+        edPasswd.setText(pref.getString(PREF_PASSWD, ""));
 //        btnLogin.setText("LOGIN");
     }
 
@@ -29,8 +36,16 @@ public class LoginActivity extends AppCompatActivity {
         String passwd = edPasswd.getText().toString();
         Log.d("onclick", userid);
         if (userid.equals("jack") && passwd.equals("1234")){
+            getSharedPreferences("atm",MODE_PRIVATE)
+                    .edit()
+                    .putString(PREF_USERID, userid)
+                    .putString(PREF_PASSWD, passwd)
+                    .commit();
+
             Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
-            setResult(RESULT_OK);
+            getIntent().putExtra("USERID",userid);
+            getIntent().putExtra("PASSWD", passwd);
+            setResult(RESULT_OK,getIntent());
             finish();
         }else{
             Toast.makeText(this, "登入錯誤", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -3,6 +3,7 @@ package com.tom.atm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_CODE_LOGIN = 6;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, TestActivity.class));
         if (!logon){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivityForResult(intent,REQUEST_CODE_LOGIN);
@@ -21,7 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode){
             case REQUEST_CODE_LOGIN:
-                if (resultCode != RESULT_OK){
+                if (resultCode == RESULT_OK) {
+                    String userid = data.getStringExtra("USERID");
+                    String passwd = data.getStringExtra("PASSWD");
+                    Log.d("LOGIN", userid + "/" + passwd);
+                }else{
                     finish();
                 }
                 return;
